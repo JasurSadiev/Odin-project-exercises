f0cc875c4eb5143ac177654740b106cd2fe59d3c
Jasurbek <goldenwashington159@gmail.com>
2022-10-24 13:08:35 +0600

    User signUp, signIn, and signOut has been created

--- JavaScript/game-guide/scripts/auth.js
+++ JavaScript/game-guide/scripts/auth.js
@@ -1,5 +1,20 @@
-// signup
 const auth = firebase.auth();
+const db = firebase.firestore();
+// Get data
+db.collection("guides").get().then(snapshot => {
+    setupGuides(snapshot.docs);
+});
+
+// listen for auth state changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+
+    } else {
+
+    };
+});
+
+// signup
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -10,6 +25,34 @@ signupForm.addEventListener("submit", (e) => {
     
     // sign up user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred);
+        // console.log(cred.user);
+        const modal = document.querySelector("#modal-signup");
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
     })
 });
+
+// Logout 
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth.signOut().then( () => {
+        console.log("Successfully logout!");
+    })
+});
+
+// Login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Get user credentials
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log("Successfully signed in! " + JSON.stringify(cred.user));
+        const modal = document.querySelector("#modal-login");
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+})
--- JavaScript/game-guide/scripts/index.js
+++ JavaScript/game-guide/scripts/index.js
@@ -1,3 +1,19 @@
+// Variables
+const guideList = document.querySelector(".guides");
+
+// setup guides
+const setupGuides = (data) => {
+  let html = '';
+  data.forEach(doc => {
+    const guide  = doc.data();
+    console.log(guide);
+  });
+};
+
+
+
+
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
