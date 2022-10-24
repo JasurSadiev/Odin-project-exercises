e14a74affc8f36f40e89a0798e50e186af3f3d96
Jasurbek <goldenwashington159@gmail.com>
2022-10-24 19:38:59 +0600

    Writing and Reading a data from the db

--- JavaScript/game-guide/scripts/auth.js
+++ JavaScript/game-guide/scripts/auth.js
@@ -1,18 +1,33 @@
-const auth = firebase.auth();
-const db = firebase.firestore();
-// Get data
-db.collection("guides").get().then(snapshot => {
-    setupGuides(snapshot.docs);
-});
-
 // listen for auth state changes
 auth.onAuthStateChanged(user => {
     if (user) {
-
+        db.collection("guides").onSnapshot(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        }, err => {
+            console.log(err.message);
+        });
     } else {
-
+        setupUI();
+        setupGuides([]);
     };
 });
+
+// Create new guide
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    db.collection("guides").add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        const modal = document.querySelector("#modal-create");
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err.message);
+    })
+})
 
 // signup
 const signupForm = document.querySelector("#signup-form");
@@ -25,11 +40,15 @@ signupForm.addEventListener("submit", (e) => {
     
     // sign up user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value
+        })
         // console.log(cred.user);
+    }).then(() => {
         const modal = document.querySelector("#modal-signup");
         M.Modal.getInstance(modal).close();
         signupForm.reset();
-    })
+    });
 });
 
 // Logout 
--- JavaScript/game-guide/scripts/index.js
+++ JavaScript/game-guide/scripts/index.js
@@ -1,13 +1,53 @@
 // Variables
+// const db = firebase.firestore();
 const guideList = document.querySelector(".guides");
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+const accountDetails = document.querySelector(".account-details");
+
+const setupUI = (user) => {
+  if (user) {
+    // account info
+    db.collection('users').doc(user.uid).get().then( doc => {
+      const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+      `;
+      accountDetails.innerHTML = html;
+    })
+
+    //toggle UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // hide account info
+    accountDetails.innerHTML = "";
+
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+}
 
 // setup guides
 const setupGuides = (data) => {
-  let html = '';
-  data.forEach(doc => {
-    const guide  = doc.data();
-    console.log(guide);
-  });
+  if (data.length){
+
+    let html = '';
+    data.forEach(doc => {
+      const guide  = doc.data();
+      const li = `
+      <li>
+      <div class="collapsible-header grey lighten-4">${guide.title}</div>
+      <div class="collapsible-body white">${guide.content}</div>
+      </li>
+      `;
+      html += li;
+    });
+    
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML = `<h5 class="center-align">Login to see game guides!</h5>`
+  }
 };
 
 
