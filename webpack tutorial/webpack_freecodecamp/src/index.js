019c4f32f794a97097140457911454db640665cb
Jasurbek <goldenwashington159@gmail.com>
2022-10-31 20:38:16 +0600

    Add SASS loader override Bootstrap colors

--- webpack tutorial/webpack_freecodecamp/src/index.js
+++ webpack tutorial/webpack_freecodecamp/src/index.js
@@ -1,7 +1,7 @@
 import { AlertService } from "./app/alert.service";
 import { run } from "./app/app";
 import { ComponentService } from "./app/component.service";
-import "../src/main.css";
+import "../src/main.scss";
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
