209dd9123a6f3747dc6c08e33dc73a5236f35343
Jasurbek <goldenwashington159@gmail.com>
2022-10-31 22:40:07 +0600

    Add html-loader, file-loader, and clean-webpack-plugin

--- webpack tutorial/webpack_freecodecamp/webpack.prod.js
+++ webpack tutorial/webpack_freecodecamp/webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = merge(common, {
 	mode: "production",
 	// devtool: "none",
 	output: {
-		filename: "main.[contentHash].js",
+		filename: "main.[contenthash].js",
 		path: path.resolve(__dirname, "dist"),
+		assetModuleFilename: "images/[name].[hash][ext]",
+		clean: true,
 	},
+	plugins: [new CleanWebpackPlugin()],
 });
