dbde9efa20db62f576aa4b85cc30dbf81d739fa2
Jasurbek <goldenwashington159@gmail.com>
2022-10-31 20:28:58 +0600

    Add first loader to handle CSS

--- webpack tutorial/webpack_freecodecamp/src/index.js
+++ webpack tutorial/webpack_freecodecamp/src/index.js
@@ -1,6 +1,7 @@
 import { AlertService } from "./app/alert.service";
 import { run } from "./app/app";
 import { ComponentService } from "./app/component.service";
+import "../src/main.css";
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
