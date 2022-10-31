7c13a48aeb3e5a79df5ec4182ed6e0686eafba71
Jasurbek <goldenwashington159@gmail.com>
2022-10-31 20:02:23 +0600

    Webpack bundling app code

--- webpack tutorial/webpack_freecodecamp/src/index.js
+++ webpack tutorial/webpack_freecodecamp/src/index.js
@@ -1,1 +1,6 @@
-alert("Hello from Webpack");
+import { AlertService } from "./app/alert.service";
+import { run } from "./app/app";
+import { ComponentService } from "./app/component.service";
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
