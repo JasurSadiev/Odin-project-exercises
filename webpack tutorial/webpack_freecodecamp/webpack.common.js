5d5db0a13fb45244ff03bb7f58dbffda7807588b
Jasurbek <goldenwashington159@gmail.com>
2022-10-31 21:23:45 +0600

    Adding production and development configs

--- webpack tutorial/webpack_freecodecamp/webpack.common.js
+++ webpack tutorial/webpack_freecodecamp/webpack.common.js
@@ -1,13 +1,8 @@
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-	mode: "development",
 	// devtool: "none",
 	entry: "./src/index.js",
-	output: {
-		filename: "main.[contentHash].js",
-		path: path.resolve(__dirname, "dist"),
-	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./src/template.html",
