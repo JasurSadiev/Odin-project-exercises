2a2076ee5e56f9a47ef295e423a199daafac7d8a
Jasurbek <goldenwashington159@gmail.com>
2022-10-31 20:59:51 +0600

    Cache, Busting and HtmlWebpackPlugin

--- webpack tutorial/webpack_freecodecamp/src/index.js
+++ webpack tutorial/webpack_freecodecamp/src/index.js
@@ -2,6 +2,7 @@ import { AlertService } from "./app/alert.service";
 import { run } from "./app/app";
 import { ComponentService } from "./app/component.service";
 import "../src/main.scss";
+console.log("Hello");
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
