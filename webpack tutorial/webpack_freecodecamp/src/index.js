3b41cfc551482f5f6e04b9a0db382c8c3ebd60f5
Jasurbek <goldenwashington159@gmail.com>
2022-11-01 14:08:15 +0600

    Created Joke Generator using Webpack5

--- webpack tutorial/webpack_freecodecamp/src/index.js
+++ webpack tutorial/webpack_freecodecamp/src/index.js
@@ -4,5 +4,4 @@ import { ComponentService } from "./app/component.service";
 import "../src/main.scss";
 const alertService = new AlertService();
 const componentService = new ComponentService();
-
 run(alertService, componentService);
