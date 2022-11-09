fa8cdbecc76af4f4ef51b2291092e1a8ea09d949
Jasurbek <goldenwashington159@gmail.com>
2022-11-09 22:37:30 +0600

    Generating a new gif everytime button is clicked

--- JavaScript/random_gif/script.js
+++ JavaScript/random_gif/script.js
@@ -1,15 +1,35 @@
 const img = document.querySelector("img");
+const button = document.querySelector("#button");
+// function displayGif() {
+// 	fetch(
+// 		"https://api.giphy.com/v1/gifs/translate?api_key=8pM558k3lIVq1BEZV2OiRlCUTrDDPcUs&s=cats",
+// 		{
+// 			mode: "cors",
+// 		}
+// 	)
+// 		.then(function (response) {
+// 			return response.json();
+// 		})
+// 		.then(function (response) {
+// 			img.src = "";
+// 			console.log(response.data.images.original.url);
+// 			img.src = response.data.images.original.url;
+// 		});
+// }
 
-fetch(
-	"https://api.giphy.com/v1/gifs/translate?api_key=8pM558k3lIVq1BEZV2OiRlCUTrDDPcUs&s=cats",
-	{
-		mode: "cors",
-	}
-)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (response) {
-		// console.log(response.data.images.original.url);
-		img.src = response.data.images.original.url;
-	});
+button.addEventListener("click", () => {
+	fetch(
+		"https://api.giphy.com/v1/gifs/translate?api_key=8pM558k3lIVq1BEZV2OiRlCUTrDDPcUs&s=cats",
+		{
+			mode: "cors",
+		}
+	)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (response) {
+			img.src = "";
+			console.log(response.data.images.original.url);
+			img.src = response.data.images.original.url;
+		});
+});
