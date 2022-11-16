ddb9edbda3580ee4572c230ea664e82f9acaf6b8
Jasurbek <goldenwashington159@gmail.com>
2022-11-16 21:49:15 +0600

    Optimizing the functions to work faster

--- JavaScript/Recursive_Functions/script.js
+++ JavaScript/Recursive_Functions/script.js
@@ -2,36 +2,29 @@
 const inputText1 = document.getElementsByClassName("input-text");
 const inputButton1 = document.getElementsByClassName("input-button");
 const screen = document.querySelector("#screen");
-let total = 1;
+le;
 
 // SumUp function
 function sumUp(num) {
-	// console.log(num);
 	if (num <= 1) {
-		return total;
+		return num;
 	}
-	total += num;
-	num--;
-	return sumUp(num);
+	return num + sumUp(num - 1);
 }
 
-// PowerUp function
 function powerUp(base, exponent) {
 	if (exponent <= 0) {
-		return total;
+		return 1;
 	}
-	total = total * base;
-	exponent--;
-	return powerUp(base, exponent);
+	return base * powerUp(base, exponent - 1);
 }
 
 // Get Factorial
 function getFactorial(int) {
 	if (int <= 1) {
 		return 1;
-	} else {
-		return int * getFactorial(int - 1);
 	}
+	return int * getFactorial(int - 1);
 }
 
 // Event Listeners
@@ -41,7 +34,6 @@ inputButton1[0].addEventListener("click", (e) => {
 		parseInt(inputText1[0].value)
 	)}`;
 	// console.log(inputText1[1]);
-	total = 1;
 });
 
 inputButton1[1].addEventListener("click", (e) => {
@@ -52,7 +44,6 @@ inputButton1[1].addEventListener("click", (e) => {
 		parseInt(inputText1[1].value[0]),
 		parseInt(inputText1[1].value[2])
 	)}`;
-	total = 1;
 });
 
 inputButton1[2].addEventListener("click", (e) => {
