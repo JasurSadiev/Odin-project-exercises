72f475d6fe9e740b7b28c3e9271adc13cb636675
Jasurbek <goldenwashington159@gmail.com>
2022-11-30 13:46:20 +0600

    Executing my first function in React JS :)

--- React_Journey/My_first_reactApp/my-first-react-app/src/App.js
+++ React_Journey/My_first_reactApp/my-first-react-app/src/App.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
+	function formateName(obj) {
+		return obj.firstName + " " + obj.lastName;
+	}
+
+	const user = {
+		firstName: "Jasurbek",
+		lastName: "Sadiev",
+	};
 	return (
 		<div className="App">
-			<h1>Hello, World!</h1>
+			<h1>Hello, {formateName(user)}!</h1>
 		</div>
 	);
 }
