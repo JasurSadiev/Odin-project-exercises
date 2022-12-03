3c4a4639c86f5c847765abe4bbd7081eed3bba4f
Jasurbek <goldenwashington159@gmail.com>
2022-12-03 21:11:23 +0600

    Fetching data from API

--- React_Journey/50_React_Projects_Challenges/movie-app/src/App.js
+++ React_Journey/50_React_Projects_Challenges/movie-app/src/App.js
@@ -1,7 +1,18 @@
+import React, { useEffect } from "react";
 import "./App.css";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=3a68e8a2";
+const App = () => {
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
+
+		console.log(data);
+	};
+	useEffect(() => {
+		searchMovies("After");
+	}, []);
 	return <div className="App"></div>;
-}
+};
 
 export default App;
