4bf2d194c8651395a593a047ff175332d4c0fcce
Jasurbek <goldenwashington159@gmail.com>
2022-12-04 20:52:03 +0600

    Pages and routes are setup

---
+++ React_Journey/50_React_Projects_Challenges/JS_blog/client/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+	return (
+		<div className="auth">
+			<h1>Login</h1>
+			<form>
+				<input type="text" placeholder="username..." />
+				<input type="password" placeholder="password..." />
+			</form>
+		</div>
+	);
+};
+
+export default Login;
