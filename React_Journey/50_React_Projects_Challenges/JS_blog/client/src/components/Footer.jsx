29bc1ee0aa666ddd6fb450783c314c80e86d1bd3
Jasurbek <goldenwashington159@gmail.com>
2022-12-05 19:41:26 +0600

    Home Page design is done

--- React_Journey/50_React_Projects_Challenges/JS_blog/client/src/components/Footer.jsx
+++ React_Journey/50_React_Projects_Challenges/JS_blog/client/src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../images/logo.png";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<img src={Logo} alt='logo' />
+			<span>
+				Made with ❤️ and <b>React.js</b>
+			</span>
+		</footer>
+	);
 };
 
 export default Footer;
--- React_Journey/50_React_Projects_Challenges/JS_blog/client/src/pages/Home.jsx
+++ React_Journey/50_React_Projects_Challenges/JS_blog/client/src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	return <div>Home</div>;
+	const posts = [
+		{
+			id: 1,
+			title:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, voluptas!",
+			desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure aliquam vitae asperiores rerum quaerat at numquam recusandae odit, obcaecati molestiae quo maxime totam maiores earum odio, iusto ipsum eius, autem dolorem! Dolore aperiam, nobis reiciendis aliquid beatae quam aspernatur aut labore facilis nihil accusantium, corporis odit doloribus consequuntur tempora sapiente.",
+			img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		},
+	];
+	return (
+		<div className='home'>
+			<div className='posts'>
+				{posts.map((post) => (
+					<div className='post' key={post.id}>
+						<div className='img'>
+							<img src={post.img} alt='' />
+						</div>
+						<div className='content'>
+							<Link className='link' to={`/post/${post.id}`}>
+								<h1>{post.title}</h1>
+							</Link>
+							<p>{post.desc}</p>
+							<button>Read More</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Home;
