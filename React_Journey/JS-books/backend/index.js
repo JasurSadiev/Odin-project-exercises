8d3ff9ffd2e44be014123b5235bb86525c335aa1
Jasurbek <goldenwashington159@gmail.com>
2022-12-06 17:07:26 +0600

    Inserting a new data to db from backend

--- React_Journey/JS-books/backend/index.js
+++ React_Journey/JS-books/backend/index.js
@@ -6,12 +6,32 @@ const app = express();
 const db = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "qwerty1234",
+	password: "password",
 	database: "test",
+	// insecureAuth: true,
 });
 
+app.use(express.json());
 app.get("/", (req, res) => {
 	res.json("Hello this is the backend!");
+});
+
+app.get("/books", (req, res) => {
+	const q = "SELECT * FROM books";
+	db.query(q, (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
+});
+
+app.post("/books", (req, res) => {
+	const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+	const values = ["Alibaba", "Bestselling Book", "alibaba.png"];
+
+	db.query(q, [values], (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
 });
 
 app.listen(8800, () => {
