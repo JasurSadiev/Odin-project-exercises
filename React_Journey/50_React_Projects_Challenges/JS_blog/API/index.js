4d402040ce5ac1028ae344329dcb08df62eb0eaf
Jasurbek <goldenwashington159@gmail.com>
2022-12-09 18:40:22 +0600

    uploading a file to server

--- React_Journey/50_React_Projects_Challenges/JS_blog/API/index.js
+++ React_Journey/50_React_Projects_Challenges/JS_blog/API/index.js
@@ -3,11 +3,27 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+	destination: function(req, res, cb) {
+		cb(null, "../client/public/upload")
+	},
+	filename: function (req, file, cb) {
+		
+		cb(null, Date.now() + file.originalname)
+	}
+})
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), function(req, res) {
+	const file = req.file;
+	res.status(200).json(file.filename)
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
